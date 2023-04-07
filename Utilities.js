c2a47c0441addbd9eca90920977b7324a5b30c75
Giant Shell <gtk.xssh@gmail.com>
2023-04-07 15:33:39 +0200

    First Commit

---
+++ Utilities.js
@@ -0,0 +1,12 @@
+﻿function saveFile(filename, Content) {
+    var link = document.createElement('a');
+    link.download = filename;
+    link.href = "data:text/json;charset=utf-8," + encodeURIComponent(Content)
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
+
+function openFilePicker() { 
+    document.getElementById('filePicker').click();
+}
